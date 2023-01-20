94a1396540bdee8ae679d50667dfaf46d53507cd
unknown <you@example.com>
2023-01-20 13:35:46 +0530

    scope_variable

--- scope_variable.c
+++ scope_variable.c
@@ -3,10 +3,10 @@ int func();
 int var = 20;
 int main(){
     int var = 24;
-    printf("%d \n",var);
+    printf("%d \n",var);//output:24
     func();
     return 0;
 }
 int func(){
-    printf("%d",var);
+    printf("%d",var);//output:20
 }
