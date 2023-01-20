301db88500e7d54097e1b783b33e1ff9deb53dea
unknown <you@example.com>
2023-01-20 22:29:51 +0530

    Conditionals_and_loops(Solved).c

--- ConditionalsAndLoops(Solved).c
+++ ConditionalsAndLoops(Solved).c
@@ -3,7 +3,7 @@
 int main(){
     int i=1024;
     for(;i;i>>=1)
-        printf("Hello World \n");
+        printf("Hello World \n");//Output:11
 //Output of following program
 int j;
 for(j=0;j<20;j++){
@@ -13,7 +13,7 @@ for(j=0;j<20;j++){
         case 5:j += 5;
         default:j += 4;
     }
-    printf("%d ",j);
+    printf("%d ",j);//Output:16 21
     }
     // How many times will "Neso" be printed on screen
     int k=-5;
@@ -24,14 +24,28 @@ for(j=0;j<20;j++){
             k++;
             continue;
         }
-        printf("Neso");
+        printf("Neso");//Output:0 times
     }
     //find output of following program
-    int l=0;
-    for(printf("one \n");l<3 && printf("");
-    l++;
+    int m=0;
+    for(printf("\n one \n");m<3 && printf("");m++)/*m<3(0<3) is true but printf("")is 0 as it prints 
+    nothing,so the loop is not executed at all*/
     {
         printf("Hi \n");
-    }
-    return 0;
+    }//Output: one
+    // Find output of following program, taking size of unsigned int as 4 bytes ,Range 0 to 4294967295
+    unsigned int o = 500;
+    while(o++!=0);//This while loop has no body it has ended as it has ";"
+    /*from 500 to 4294.. while loop executes ,after 4294.. when 0!=0 condition 
+    is not satisfied it moves out of 
+    loop and prints"1"output because of post increment operator i++*/
+    printf("%d \n",o);//Output: 1
+//Find output of following program:-
+int x=3;
+if(x==2);//since x!=2 we come out of the loop
+x=0;//now val of x=0 as per statement
+if(x==3)x++;//0!=3
+else x+=2;//so x=x+2 is printed i.e x=0+2=2
+printf("x=%d",x);//Output:2
+return 0;
 }
