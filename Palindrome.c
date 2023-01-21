6f2b99f2be44a96f6c6c0a773293926d1d89e760
unknown <you@example.com>
2023-01-21 15:13:31 +0530

    Palindrome_Code

---
+++ Palindrome.c
@@ -0,0 +1,25 @@
+/*Palindrome is a number which if read backwards
+ gives the same number as being read forward, Eg:121, 535 
+ Idea: We need to reverse the number and if the reverse number is 
+ equal to the actual number we say that its a Palindrome number*/
+ 
+ //WAP take a Number from user and check if its a Palindrome or not
+ #include <stdio.h>
+ int main()
+ {
+    int n,rem,result=0,q;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+    q=n;
+    while(q!=0)
+    {
+        rem=q%10;
+        result=result*10+rem;
+        q=q/10;
+    }
+    if(result==n)
+        printf("Number is a Palindrome");
+    else
+        printf("Number is not a Palindrome");
+    return 0;
+}
