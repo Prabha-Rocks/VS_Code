e81af3df152fd331c50504b98f5b84997dbc18f2
unknown <you@example.com>
2023-01-24 01:00:33 +0530

    PerfectNum_Prog

---
+++ PerfectNum_prog.c
@@ -0,0 +1,25 @@
+/*Perfect Number: A Perfect Number is a +ve integer that is equal to the sum of 
+all its proper divisors excluding itself
+For eg: 6 is a perfect Number , Proper +ve divisors of 6 are 1,2,3 and its sum=1+2+3=6*/
+//WAP to check for a Perfect number
+#include <stdio.h>
+int main()
+{
+    int rem,sum=0,num,i;
+    printf("Enter a Number:");
+    scanf("%d",&num);
+    //Loop to check for a perfect number
+    for(i=1;i<num;i++)
+    {
+        rem=num%i;
+    if(rem==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum==num)
+        printf("Its a perfect number");
+    else
+        printf("Its Not a perfect number");
+    return 0;
+}
