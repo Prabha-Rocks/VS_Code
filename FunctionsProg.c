40658d2b5cc2d1b18d5ad7bf1db2ec971d81712e
unknown <you@example.com>
2023-01-25 15:01:03 +0530

    Functions.c

--- FunctionsProg.c
+++ FunctionsProg.c
@@ -3,7 +3,7 @@ perform some computation and produces output
 Syntax:ReturnType FuncName(setOfInput)*/
 // Basic Program to Calculate Area of Rectangle using function
 #include<stdio.h>
-int areaOfRect(int len,int wid)
+int areaOfRect(int len,int wid)//Function Definition 
 {
     int area;
     area=len*wid;
@@ -13,8 +13,8 @@ int main()
 {
     int l=10,b=5;
     int area=areaOfRect(l,b);
-    printf("Area:%d\n",area);
+    printf("Area:%d\n",area);//Area:50
     l=50,b=20;
     area=areaOfRect(l,b);
-    printf("Area:%d",area);
+    printf("Area:%d",area);//Area:1000
 }
